70660d6ff0c620e26d9eb8376f756cc6528a2d20
Michel Meyer <meyer_michel@outlook.com>
2021-04-06 23:54:48 +0200

    finish signature test

--- tests/conanbuilder/test_signature.py
+++ tests/conanbuilder/test_signature.py
@@ -1,13 +1,34 @@
+import pytest
 from src.conanbuilder.signature import Signature
 
 
-def test_version():
-    signature = Signature()
+@pytest.fixture
+def signature():
+    return Signature()
+
+
+def test_version(signature):
     assert signature.version is None
 
 
-def test_version_set():
-    signature = Signature()
+def test_version_set(signature):
     signature.version = "1.0.0"
     assert signature.version == "1.0.0"
 
+
+def test_channel(signature):
+    assert signature.channel is None
+
+
+def test_channel_set(signature):
+    signature.channel = "dev"
+    assert signature.channel == "dev"
+
+
+def test_user(signature):
+    assert signature.user is None
+
+
+def test_user_set(signature):
+    signature.user = "user"
+    assert signature.user == "user"
