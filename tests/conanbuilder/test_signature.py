6de7212116adb29f805cf356f21f2fce8338367e
Michel Meyer <meyer_michel@outlook.com>
2021-04-06 23:18:29 +0200

    changed to pytest

---
+++ tests/conanbuilder/test_signature.py
@@ -0,0 +1,13 @@
+from conanbuilder.signature import Signature
+
+
+def test_version():
+    signature = Signature()
+    assert signature.version is None
+
+
+def test_version_set():
+    signature = Signature()
+    signature.version = "1.0.0"
+    assert signature.version is "1.0.0"
+
