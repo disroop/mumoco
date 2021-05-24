f282968fb8f7cfd818dee643bbcdcdf015f6219f
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-05-24 10:33:54 +0200

    add test_remote.py

---
+++ tests/conanbuilder/test_remote.py
@@ -0,0 +1,16 @@
+import pytest
+from src.conanbuilder.remote import Remote
+
+
+@pytest.fixture
+def remote():
+    return Remote("myName", "myUrl")
+
+
+def test_default_values(remote):
+    assert remote.name == "myName"
+    assert remote.url == "myUrl"
+    assert remote.verify_ssl is True
+    assert remote.priority == 0
+    assert remote.force is False
+    assert remote.login is False
