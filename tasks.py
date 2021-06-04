483efb95f446f3ae2c35b92c5f77687e0b6518a9
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-04 21:04:12 +0200

    Remove poetry command from tasks.py
    Since tasks.py depends on invoke which is installed by poetry, you can't
    call poetry command in that file.

--- tasks.py
+++ tasks.py
@@ -12,23 +12,13 @@ from invoke import run, task
 
 
 @task
-def install_dependencies(c):
-    c.run("poetry install --no-interaction --no-root")
-
-
-@task
-def install_libraries(c):
-    c.run("poetry install --no-interaction")
-
-
-@task
 def check_format_with_black(c):
-    c.run("poetry run black -l 120 --check .")
+    c.run("black -l 120 --check .")
 
 
 @task
 def sort_imports_with_isort(c):
-    c.run("poetry run isort --profile black --check .")
+    c.run("isort --profile black --check .")
 
 
 @task
@@ -39,37 +29,24 @@ def lint_with_pylint(c):
 @task
 def lint_with_flake8(c):
     # stop the build if there are Python syntax errors or undefined names
-    c.run("poetry run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv")
+    c.run("flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv")
     # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
     c.run(
-        "poetry run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --extend-exclude=.venv"
+        "flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --extend-exclude=.venv"
     )
 
 
 @task
 def lint_with_bandit(c):
-    c.run("poetry run bandit -r src/ --exclude .venv")
+    c.run("bandit -r src/ --exclude .venv")
 
 
 @task
 def run_tests(c):
     run(
-        "poetry run pytest --cov=src --junitxml=reports/xunit.xml --cov src/ --cov-report term --cov-report html:reports/html --cov-report xml:reports/coverage.xml"
+        "pytest --cov=src --junitxml=reports/xunit.xml --cov src/ --cov-report term --cov-report html:reports/html --cov-report xml:reports/coverage.xml"
     )
     # todo define threshold and fail if coverage is below
-
-
-@task
-def build(c):
-    c.run("poetry build")
-
-
-@task
-def publish(c, pypiusername, pypipassword, dryrun=False):
-    if dryrun:
-        c.run(f"poetry publish --username '{pypiusername}'  --password '{pypipassword}' --dry-run")
-    else:
-        c.run(f"poetry publish --username '{pypiusername}'  --password '{pypipassword}'")
 
 
 @task
@@ -84,9 +61,3 @@ def clean(c):
     except OSError:
         pass
 
-
-@task
-def configure_poetry(c):
-    c.run("poetry config virtualenvs.path .venv")
-    c.run("poetry config virtualenvs.create true")
-    c.run("poetry config virtualenvs.in-project true")
