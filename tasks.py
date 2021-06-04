2c05c90d9c64dad952273d60a453d5b4368ab589
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-04 02:14:28 +0200

    format tasks.py

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-The build.py is the single point containing the build logic. Thus this script can be used to build locally or methods from the file can be used in the github actions.
+The build.py is the single point containing the build logic. Thus this script can be used to build locally or
+methods from the file can be used in the github actions.
 The github actions shall just call methods from this file so we are not locked in the the build server solution.
 """
 import os
