fe9212f3691cb685a77a2b3dd296a882975904f0
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-04 02:13:17 +0200

    update build.py

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 invoke --list
 
-#invoke clean
-#invoke configure_poetry
+invoke clean
+invoke configure-poetry
 invoke check-format-with-black
 invoke sort-imports-with-isort
 invoke install-dependencies
 invoke install-libraries
+invoke lint-with-pylint
 invoke lint-with-flake8
 invoke lint-with-bandit
 #invoke # todo add mypy
