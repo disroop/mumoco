06f55db29e21a661f4426fd161d12ad083c78ecc
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-05 17:20:28 +0200

    Minor cleaning of tasks.py

--- tasks.py
+++ tasks.py
@@ -5,35 +5,56 @@ The build.py is the single point containing the build logic. Thus this script ca
 methods from the file can be used in the github actions.
 The github actions shall just call methods from this file so we are not locked in the the build server solution.
 """
-import os
 import shutil
 
-from invoke import run, task
+from invoke import task
+
+LINE_LENGTH = 120
+MAX_COMPLEXITY = 10
 
 
 @task
 def check_format_with_black(c, fix=False):
+    format_cmd = "black -l {} .".format(LINE_LENGTH)
+    check_cmd = format_cmd + " --check"
     if fix:
-        c.run("black -l 120 .")
-    c.run("black -l 120 . --check")
+        c.run(format_cmd)
+    c.run(check_cmd)
 
 
 @task
-def sort_imports_with_isort(c):
-    c.run("isort --profile black --check .")
+def sort_imports_with_isort(c, fix=False):
+    isort_fix_cmd = "isort --profile black ."
+    isort_check_cmd = isort_fix_cmd + " --check"
+    if fix:
+        c.run(isort_fix_cmd)
+    c.run(isort_check_cmd)
 
 
 @task
 def lint_with_pylint(c):
-    c.run("poetry run pylint ./src --enable=W0611,W0614 -disable=C0114,C0115,C0116,W0511 --max-line-length=120")
+    enabled_warnings = ["W0611", "W0614"]
+    disabled_warnings = ["C0114", "C0115", "C0116", "W0511"]
+    pylint_cmd = "pylint ./src --enable={} -disable={} --max-line-length={}".format(
+        ",".join(enabled_warnings), ",".join(disabled_warnings), LINE_LENGTH
+    )
+    c.run(pylint_cmd)
 
 
 @task
 def lint_with_flake8(c):
+    errors = ["E9", "F63", "F7", "F82"]
     # stop the build if there are Python syntax errors or undefined names
-    c.run("flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics  --extend-exclude=.venv")
-    # exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-    c.run("flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --extend-exclude=.venv")
+    flake8_syntax_cmd = "flake8 . --count --select={}".format(",".join(errors))
+    flake8_syntax_cmd = flake8_syntax_cmd + "--show-source --statistics --extend-exclude=.venv"
+    c.run(flake8_syntax_cmd)
+
+    # exit-zero treats all errors as warnings. The GitHub editor is 120 chars wide
+    flake8_complexity_cmd = "flake8 . --count --exit-zero --max-complexity={} --max-line-length={}".format(
+        MAX_COMPLEXITY, LINE_LENGTH
+    )
+    flake8_complexity_cmd = flake8_complexity_cmd + " --statistics --extend-exclude=.venv"
+    c.run(flake8_complexity_cmd)
 
 
 @task
@@ -43,20 +64,13 @@ def lint_with_bandit(c):
 
 @task
 def run_tests(c):
-    run(
-        "pytest --cov=src --junitxml=reports/xunit.xml --cov src/ --cov-report term --cov-report html:reports/html --cov-report xml:reports/coverage.xml"
-    )
-    # todo define threshold and fail if coverage is below
+    pytest_cmd = "pytest --cov=src --junitxml=reports/xunit.xml --cov src/"
+    pytest_cmd = pytest_cmd + " --cov-report term --cov-report html:reports/html --cov-report xml:reports/coverage.xml"
+    c.run(pytest_cmd)
 
 
 @task
 def clean(c):
-    shutil.rmtree(".venv", ignore_errors=True)
-    shutil.rmtree(".pytest_cache", ignore_errors=True)
-    shutil.rmtree(".mypy_cache", ignore_errors=True)
-    shutil.rmtree("dist", ignore_errors=True)
-    shutil.rmtree("reports", ignore_errors=True)
-    try:
-        os.remove(".coverage")
-    except OSError:
-        pass
+    folders = [".venv", ".pytest_cache", ".mypy_cache", "dist", "reports", "coverage"]
+    for folder in folders:
+        shutil.rmtree(folder, ignore_errors=True)
