38b3e79b9ba5d1cf5c0a0b81dfc8000c91a76be5
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-12 20:58:55 +0200

    Fix error detected by deepsource
    Rename conanfactory -> conan_factory

--- tests/conanbuilder/test_package.py
+++ tests/conanbuilder/test_package.py
@@ -25,34 +25,33 @@ def inspect_conanfile_only_name(*args, **kwargs):
     attribute = attributes_list[0]
     if attribute in vals:
         return vals[attribute]
-    else:
-        raise ConanException
+    raise ConanException
 
 
 def test_get_path():
-    conanfactory = Mock()
+    conan_factory = Mock()
     signature = Signature()
-    package = Package(conanfactory, signature, "dir")
+    package = Package(conan_factory, signature, "dir")
     assert package.get_path() == "dir/"
 
 
 def test_get_pattern_in_file():
-    conanfactory, _, _ = Conan.factory()
+    conan_factory, _, _ = Conan.factory()
     signature = Signature()
     signature.version = "1.0.0"
     signature.channel = "dev"
     signature.user = "disroop"
-    conanfactory.inspect = MagicMock(side_effect=inspect_conanfile_all)
-    package = Package(conanfactory, signature, "dir")
+    conan_factory.inspect = MagicMock(side_effect=inspect_conanfile_all)
+    package = Package(conan_factory, signature, "dir")
     assert package.get_pattern() == "example/0.0.1@test/release"
 
 
 def test_get_pattern_by_signature():
-    conanfactory, _, _ = Conan.factory()
+    conan_factory, _, _ = Conan.factory()
     signature = Signature()
     signature.version = "1.0.0"
     signature.channel = "dev"
     signature.user = "disroop"
-    conanfactory.inspect = MagicMock(side_effect=inspect_conanfile_only_name)
-    package = Package(conanfactory, signature, "dir")
+    conan_factory.inspect = MagicMock(side_effect=inspect_conanfile_only_name)
+    package = Package(conan_factory, signature, "dir")
     assert package.get_pattern() == "example/1.0.0@disroop/dev"
