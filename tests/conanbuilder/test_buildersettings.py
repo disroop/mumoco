64d3dfc767d4552e48937f3908f1fd36ced4c36a
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-19 13:03:51 +0200

    Move all tests for BuilderSettings into the appropriate file

--- tests/conanbuilder/test_buildersettings.py
+++ tests/conanbuilder/test_buildersettings.py
@@ -1,5 +1,16 @@
 from src.conanbuilder.buildersettings import BuilderSettings
 
+def test_convert_build_settings_str_empty():
+    builder_settings = BuilderSettings()
+    settings_str = builder_settings.convert_build_settings_str()
+    assert settings_str == ""
+
+
+def test_convert_build_settings_str():
+    builder_settings = BuilderSettings(build_settings=["a", "b", "c"])
+    settings_str = builder_settings.convert_build_settings_str()
+    assert settings_str == "abc"
+
 
 def test_buildersettings_convert_to_string():
     config = BuilderSettings(
