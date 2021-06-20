5779e04e440150c7d2f90de0d6ad69f211111085
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-20 21:14:43 +0200

    test_source: fix mumoco_api call. Test fails!!

--- tests/integrationtests/source/test_source.py
+++ tests/integrationtests/source/test_source.py
@@ -6,23 +6,21 @@ import subprocess
 
 from src.mumoco import mumoco_api
 
-file_paths = os.path.dirname(os.path.abspath(__file__))
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-# setup:
-# mkdir sourcetest_src_in_git && cd  sourcetest_src_in_git && conan new sourcetest_src_in_git/1.0 -t
 def test_sources() -> None:
-    # fixme this fails
-    # mumoco_api(sources=True,
-    #            root=f"{file_paths}/sourcetest_src_in_git",
-    #            config_file_path=f"{file_paths}/config-build.json")
-
-    # but this works
     with tempfile.TemporaryDirectory() as temp_dir:
-        subprocess.run(
-            ["conan", "source", "sourcetest_src_in_git/conanfile.py", "--source-folder={}/source".format(temp_dir)],
-            check=True,
-        )
+        # subprocess.run(
+        #     ["conan", "source", "sourcetest_src_in_git/conanfile.py", "--source-folder={}/source".format(temp_dir)],
+        #     check=True,
+        # )
+
+        mumoco_api(sources=True,
+                   source_folder="{}/source".format(temp_dir),
+                   root="f{file_paths}/sourcetest_src_in_git",
+                   config_file_path=f"{FILE_PATH}/config-build.json"
+                   )
 
         assert os.path.isfile("{}/source/hello/.gitignore".format(temp_dir))
         assert os.path.isfile("{}/source/hello/CMakeLists.txt".format(temp_dir))
