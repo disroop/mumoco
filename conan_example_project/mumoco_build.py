4b2b04f19d43653f212bcb2a76c8c2609ed88512
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-28 23:34:50 +0200

    conan_example_project mumoco_build.py add cleanup

--- conan_example_project/mumoco_build.py
+++ conan_example_project/mumoco_build.py
@@ -1,15 +1,21 @@
 import os
 import tempfile
 
+from conans.client.conan_api import Conan
+
 from mumoco import MumocoAPI
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
+def cleanup():
+    conan_factory, _, _ = Conan.factory()
+    conan_factory.remove(pattern="demo*", force=True)
+
+
 if __name__ == "__main__":
+    cleanup()
     with tempfile.TemporaryDirectory() as temp_dir:
-        # run this before
-        # os.run("conan export Base  disroopbase/0.1@disroop/development")
         api = MumocoAPI(root=f"{FILE_PATH}", config_file_path=f"{FILE_PATH}/config-build.json")
         # api.sources(f"{temp_dir}/source")
         # fixme not yet working
