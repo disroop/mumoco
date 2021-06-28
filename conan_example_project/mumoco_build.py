1a357252eccfcb086e0bdcef74b85c3d798048ee
Stefan Eicher <stefan.eicher@gmail.com>
2021-06-28 23:39:31 +0200

    conan_example_project cleanup

--- conan_example_project/mumoco_build.py
+++ conan_example_project/mumoco_build.py
@@ -8,7 +8,7 @@ from mumoco import MumocoAPI
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def cleanup():
+def cleanup() -> None:
     conan_factory, _, _ = Conan.factory()
     conan_factory.remove(pattern="demo*", force=True)
 
@@ -17,7 +17,4 @@ if __name__ == "__main__":
     cleanup()
     with tempfile.TemporaryDirectory() as temp_dir:
         api = MumocoAPI(root=f"{FILE_PATH}", config_file_path=f"{FILE_PATH}/config-build.json")
-        # api.sources(f"{temp_dir}/source")
-        # fixme not yet working
-        # open in in vscode
         api.create()
