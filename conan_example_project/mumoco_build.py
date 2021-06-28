cc6439c5b8f1145dfb0cb5b01e1b51f592398090
Arnaud Gelas <arnaudgelas@gmail.com>
2021-06-28 20:16:12 +0200

    Simplify import of MumocoAPI

--- conan_example_project/mumoco_build.py
+++ conan_example_project/mumoco_build.py
@@ -1,9 +1,10 @@
 import os
 import tempfile
 
-from mumoco_api import MumocoAPI
+from mumoco import MumocoAPI
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+
 
 if __name__ == "__main__":
     with tempfile.TemporaryDirectory() as temp_dir:
