dfc3d9880525dccac26188fd98a4f96da38528b9
Michel Meyer <meyer_michel@outlook.com>
2021-09-29 22:00:56 +0200

    update to 0.3.3

--- mumoco/__init__.py
+++ mumoco/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # Re-exports objects from underlying packages (so that we can use
 # mumoco.ConfigReader instead of mumoco.conanbuilder.ConfigReader).
